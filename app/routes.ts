08d2f8348144dcef30b49d600c46a514c66d1bf8
ronald <ronald.dimatangal@gmail.comm>
2026-01-30 09:16:07 +0800

    deploying

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,25 @@
+// import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
+
+
+// export default [
+//   layout("routes/layout.tsx", [
+//     index("routes/home.tsx"),
+//     route("login", "routes/login.tsx"),
+//     route("register", "routes/register.tsx"),
+//     route("create", "routes/create.tsx"),
+//     route("manage", "routes/manage.tsx"),
+//   ])
+// ] satisfies RouteConfig;
+
 import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 export default [
   layout("routes/layout.tsx", [
-    index("routes/home.tsx"),
-    route("login", "routes/login.tsx"),
-    route("register", "routes/register.tsx"),
-    route("create", "routes/create.tsx"),
-    route("manage", "routes/manage.tsx"),
+    index("home.tsx"),
+    route("login", "login.tsx"),
+    route("register", "register.tsx"),
+    route("create", "create.tsx"),
+    route("manage", "manage.tsx"),
   ])
 ] satisfies RouteConfig;
+
